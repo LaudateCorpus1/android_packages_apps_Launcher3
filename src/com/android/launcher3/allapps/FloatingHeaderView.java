LawnchairLauncher/lawnchair

commit c6995f3eb21ab1d1a0dd548d49c5758343a3d608

[Search] Resolve work/personal tab overlap with A-Z list
This issue was caused due to launcher falling out of sync with DEVICE_SEARCH_FLAG. This change makes AllApps views agnostic of search input type when rendering layout.

Bug: 182591985
Test: local
Change-Id: Ic015b68e110eea9ab5184325f2a3633a5c7d15cc

src/com/android/launcher3/allapps/FloatingHeaderView.java
@@ -106,8 +106,8 @@ public class FloatingHeaderView extends LinearLayout implements
 
     public FloatingHeaderView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        mHeaderTopPadding = FeatureFlags.ENABLE_DEVICE_SEARCH.get() ? 0 :
-                context.getResources().getDimensionPixelSize(R.dimen.all_apps_header_top_padding);
+        mHeaderTopPadding = context.getResources()
+                .getDimensionPixelSize(R.dimen.all_apps_header_top_padding);
     }
 
     @Override
@@ -125,7 +125,6 @@ public class FloatingHeaderView extends LinearLayout implements
             }
         }
         mFixedRows = rows.toArray(new FloatingHeaderRow[rows.size()]);
-        setPadding(0, mHeaderTopPadding, 0, 0);
         mAllRows = mFixedRows;
     }
 
@@ -243,9 +242,7 @@ public class FloatingHeaderView extends LinearLayout implements
 
     public int getMaxTranslation() {
         if (mMaxTranslation == 0 && mTabsHidden) {
-            int paddingOffset = getResources().getDimensionPixelSize(
-                    R.dimen.all_apps_search_bar_bottom_padding);
-            return FeatureFlags.ENABLE_DEVICE_SEARCH.get() ? 0 : paddingOffset;
+            return getResources().getDimensionPixelSize(R.dimen.all_apps_search_bar_bottom_padding);
         } else if (mMaxTranslation > 0 && mTabsHidden) {
             return mMaxTranslation + getPaddingTop();
         } else {

res/layout/all_apps_tabs.xml
@@ -26,6 +26,7 @@
     android:clipChildren="true"
     android:clipToPadding="false"
     android:descendantFocusability="afterDescendants"
+    android:paddingTop="@dimen/all_apps_header_top_padding"
     launcher:pageIndicator="@+id/tabs" >
 
     <include layout="@layout/all_apps_rv_layout" />

src/com/android/launcher3/allapps/AllAppsContainerView.java
@@ -583,10 +583,6 @@ public class AllAppsContainerView extends SpringRelativeLayout implements DragSo
         int padding = mHeader.getMaxTranslation();
         for (int i = 0; i < mAH.length; i++) {
             mAH[i].padding.top = padding;
-            if (FeatureFlags.ENABLE_DEVICE_SEARCH.get() && mUsingTabs) {
-                //add extra space between tabs and recycler view
-                mAH[i].padding.top += mLauncher.getDeviceProfile().edgeMarginPx;
-            }
             mAH[i].applyPadding();
         }
     }

src/com/android/launcher3/allapps/AllAppsPagedView.java
@@ -23,8 +23,6 @@ import android.util.AttributeSet;
 
 import com.android.launcher3.Launcher;
 import com.android.launcher3.PagedView;
-import com.android.launcher3.R;
-import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.workprofile.PersonalWorkPagedView;
 
 /**
@@ -43,10 +41,6 @@ public class AllAppsPagedView extends PersonalWorkPagedView {
 
     public AllAppsPagedView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        int topPadding = FeatureFlags.ENABLE_DEVICE_SEARCH.get() ? 0
-                : context.getResources().getDimensionPixelOffset(
-                        R.dimen.all_apps_header_top_padding);
-        setPadding(0, topPadding, 0, 0);
     }
 
     @Override

LaudateCorpus1/android_packages_apps_Launcher3

commit e2c3ed013f4d914265bd2d363df4a3886ef6425f

Launcher3: fix all app search overlap
* add margin top for RecyclerView layout (all_apps_search_bar_field_height / 2)
* fix non-tabs view max translation value inconsistency introduced in c6995f3eb21ab1d1a0dd548d49c5758343a3d608

Change-Id: Iea89e7d62f395f10aa40c331747a778a51814150
Signed-off-by: neobuddy89 <neobuddy89@gmail.com>

res/layout/all_apps_rv_layout.xml
@@ -22,4 +22,5 @@
     android:layout_below="@id/search_container_all_apps"
     android:clipToPadding="false"
     android:descendantFocusability="afterDescendants"
-    android:focusable="true" />
+    android:focusable="true"
+    android:layout_marginTop="24dp" />

src/com/android/launcher3/allapps/FloatingHeaderView.java
@@ -263,7 +263,7 @@ public class FloatingHeaderView extends LinearLayout implements
 
     public int getMaxTranslation() {
         if (mMaxTranslation == 0 && mTabsHidden) {
-            return getResources().getDimensionPixelSize(R.dimen.all_apps_search_bar_bottom_padding);
+            return 0;
         } else if (mMaxTranslation > 0 && mTabsHidden) {
             return mMaxTranslation + getPaddingTop();
         } else {